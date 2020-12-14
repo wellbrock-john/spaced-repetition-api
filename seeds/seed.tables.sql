d4f4e1eb1c008a30b94fd5e1ad4845e8e2306c72
John Wellbrock <wellbrock23john@gmail.com>
2020-12-14 11:47:41 -0700

    updated seed file to use Latin

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -10,25 +10,27 @@ VALUES
   (
     1,
     'admin',
-    'Dunder Mifflin Admin',
+    'John Wellbrock Admin',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Latin', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'ad infinitum', 'to infinity', 2),
+  (2, 1, 'antebellum', 'before the war', 3),
+  (3, 1, 'aurora borealis', 'northern lights', 4),
+  (4, 1, 'cum laude', 'with honor', 5),
+  (5, 1, 'curriculum vitae', 'course of life', 6),
+  (6, 1, 'deus ex machina', 'God from the machine', 7),
+  (7, 1, 'emeritus', 'having served one''s time', 8),
+  (8, 1, 'ex nihilo', 'out of nothing', 9),
+  (9, 1, 'magnum opus', 'great work', 10),
+  (10, 1, 'in vitro', 'in glass', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
