a913eeb6ca6f89d61e6cc7c1e57a37465ad5ea0f
John Wellbrock <wellbrock23john@gmail.com>
2020-12-17 22:21:52 -0700

    deployment ready

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,4 @@ module.exports = {
 	port: process.env.MIGRATION_DATABASE_PORT,
 	database: process.env.MIGRATION_DATABASE_NAME,
 	username: process.env.MIGRATION_DATABASE_USER,
-	password: process.env.MIGRATION_DATABASE_PASS,
 };
